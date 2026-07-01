aa71e55eff36917cfa4960be90e5e4a78e2fe6c4
afterwhy <afterwhy@gmail.com>
2026-07-01 17:01:03 +0300

    feat: non-ASCII support with segment-boundary abbreviation matching

--- lua/cmp_hledger/init.lua
+++ lua/cmp_hledger/init.lua
@@ -5,23 +5,16 @@ local util = require('cmp_hledger.util')
 source.new = function()
   local self = setmetatable({}, { __index = source })
   self.items = nil
+  self._cached_path = nil
+  self._cached_mtime = nil
   return self
 end
 
+source.get_keyword_pattern = function()
+  return '[[:lower:][:upper:]0-9_.:-]*'
+end
+
 source.get_trigger_characters = function()
-<<<<<<< Updated upstream
-  return {
-    'Ex',
-    'In',
-    'As',
-    'Li',
-    'Eq',
-    'E:',
-    'I:',
-    'A:',
-    'L:',
-  }
-=======
   if vim.bo.filetype ~= 'ledger' then
     return {}
   end
@@ -48,19 +41,14 @@ source.get_trigger_characters = function()
   end
 
   return triggers
->>>>>>> Stashed changes
 end
 
 local get_items = function(account_path)
   local openPop = assert(io.popen(vim.b.hledger_bin .. ' accounts -f ' .. account_path))
   local output = openPop:read('*all')
   openPop:close()
-<<<<<<< Updated upstream
-  local t = util.split(output, '\n')
-=======
   local t = util.split(output, "\n")
 
->>>>>>> Stashed changes
   local items = {}
   for _, s in pairs(t) do
     table.insert(items, {
@@ -68,6 +56,7 @@ local get_items = function(account_path)
       kind = cmp.lsp.CompletionItemKind.Property,
     })
   end
+
   return items
 end
 
@@ -76,35 +65,27 @@ source.complete = function(self, request, callback)
     callback()
     return
   end
-  if vim.fn.executable('hledger') == 1 then
-    vim.b.hledger_bin = 'hledger'
-  elseif vim.fn.executable('ledger') == 1 then
-    vim.b.hledger_bin = 'ledger'
+  if vim.fn.executable("hledger") == 1 then
+    vim.b.hledger_bin = "hledger"
+  elseif vim.fn.executable("ledger") == 1 then
+    vim.b.hledger_bin = "ledger"
   else
     vim.api.nvim_echo({
-      { 'cmp_hledger', 'ErrorMsg' },
-      { ' ' .. "Can't find hledger or ledger" },
+      { 'cmp_hledger',                         'ErrorMsg' },
+      { ' ' .. 'Can\'t find hledger or ledger' },
     }, true, {})
     callback()
     return
   end
   local account_path = vim.api.nvim_buf_get_name(0)
-  if not self.items then
+  local mtime = vim.fn.getftime(account_path)
+  if not self.items or self._cached_path ~= account_path or self._cached_mtime ~= mtime then
     self.items = get_items(account_path)
+    self._cached_path = account_path
+    self._cached_mtime = mtime
   end
 
   local input = util.ltrim(request.context.cursor_before_line):lower()
-<<<<<<< Updated upstream
-  local pattern, prefix_mode = util.build_pattern(input)
-  local items = {}
-  if prefix_mode then
-    items = util.filter_prefix_mode(
-      self.items, pattern, input,
-      request.context.cursor.row,
-      request.context.cursor.col,
-      request.offset
-    )
-=======
   local prefixes = util.split(input, ":")
   local is_abbrev = #prefixes > 1
 
@@ -112,7 +93,6 @@ source.complete = function(self, request, callback)
   if is_abbrev then
     items = util.filter_prefix_mode(self.items, prefixes, input,
       request.context.cursor.row, request.context.cursor.col, request.offset)
->>>>>>> Stashed changes
   else
     items = util.filter_simple_mode(self.items, input)
   end
