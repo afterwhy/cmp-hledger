c2738a2f6253b669eee76dc4992f60a1ec5873e7
afterwhy <afterwhy@gmail.com>
2026-07-02 16:16:35 +0300

    chore: revert unnecessary quote style changes and blank line to match original conventions

--- lua/cmp_hledger/init.lua
+++ lua/cmp_hledger/init.lua
@@ -35,7 +35,7 @@ local get_items = function(account_path)
   local openPop = assert(io.popen(vim.b.hledger_bin .. ' accounts -f ' .. account_path))
   local output = openPop:read('*all')
   openPop:close()
-  local t = util.split(output, "\n")
+  local t = util.split(output, '\n')
 
   local items = {}
   for _, s in pairs(t) do
@@ -44,7 +44,6 @@ local get_items = function(account_path)
       kind = cmp.lsp.CompletionItemKind.Property,
     })
   end
-
   return items
 end
 
@@ -53,14 +52,14 @@ source.complete = function(self, request, callback)
     callback()
     return
   end
-  if vim.fn.executable("hledger") == 1 then
-    vim.b.hledger_bin = "hledger"
-  elseif vim.fn.executable("ledger") == 1 then
-    vim.b.hledger_bin = "ledger"
+  if vim.fn.executable('hledger') == 1 then
+    vim.b.hledger_bin = 'hledger'
+  elseif vim.fn.executable('ledger') == 1 then
+    vim.b.hledger_bin = 'ledger'
   else
     vim.api.nvim_echo({
       { 'cmp_hledger', 'ErrorMsg' },
-      { ' ' .. "Can't find hledger or ledger" },
+      { ' ' .. 'Can\'t find hledger or ledger' },
     }, true, {})
     callback()
     return
