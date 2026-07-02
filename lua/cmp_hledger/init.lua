d79ccf78bc95878d3435cf1acfd966b6f9ed25ff
afterwhy <afterwhy@gmail.com>
2026-07-02 16:23:24 +0300

    refactor: remove get_trigger_characters, rely on keyword pattern for auto-triggering

--- lua/cmp_hledger/init.lua
+++ lua/cmp_hledger/init.lua
@@ -12,25 +12,6 @@ source.get_keyword_pattern = function()
   return '[[:lower:][:upper:]0-9_.-]*'
 end
 
-source.get_trigger_characters = function()
-  if vim.bo.filetype ~= 'ledger' then
-    return {}
-  end
-
-  local triggers = {}
-
-  for cp = string.byte('0'), string.byte('9') do table.insert(triggers, string.char(cp)) end
-  for cp = string.byte('A'), string.byte('Z') do table.insert(triggers, string.char(cp)) end
-  for cp = string.byte('a'), string.byte('z') do table.insert(triggers, string.char(cp)) end
-
-  for cp = vim.fn.char2nr('А'), vim.fn.char2nr('Я') do table.insert(triggers, vim.fn.nr2char(cp)) end
-  table.insert(triggers, vim.fn.nr2char(vim.fn.char2nr('Ё')))
-  for cp = vim.fn.char2nr('а'), vim.fn.char2nr('я') do table.insert(triggers, vim.fn.nr2char(cp)) end
-  table.insert(triggers, vim.fn.nr2char(vim.fn.char2nr('ё')))
-
-  return triggers
-end
-
 local get_items = function(account_path)
   local openPop = assert(io.popen(vim.b.hledger_bin .. ' accounts -f ' .. account_path))
   local output = openPop:read('*all')
